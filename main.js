1a1e4898bdfbe2c9bb952b950cb88d8517a7b525
christian <christianherrerias@gmail.com>
2019-01-06 11:09:43 +0100

    Funcionamiento de los archivos

--- main.js
+++ main.js
@@ -1,65 +1,33 @@
-var mes_text = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+// Esto es JavaScript
 
-var dia_text = ["Dom", "Lun", "Mar", "Mie", "Juv", "Vie", "Sab"];
+alert('Esto es un evento de JavaScript');
 
-estructurar();
+function saludo()
+{
+  alert('Esto me gusta muxo');
+}
+function saludo_2()
+{
+  alert('Esto me gusta muxo V.2');
 
-function estructurar() {
-  for (m = 0; m <= 11; m++) {
-    //Mes
-    let mes = document.createElement("DIV");
-    mes.className = "mes";
-    document.body.appendChild(mes);
-    //Tabla
-    let tabla_mes = document.createElement("TABLE");
-    tabla_mes.className = "tabla_mes";
-    mes.appendChild(tabla_mes);
-    //Título
-    let titulo = document.createElement("CAPTION");
-    titulo.className = "titulo";
-    titulo.innerText = mes_text[m];
-    tabla_mes.appendChild(titulo);
-    //Cabecera
-    let cabecera = document.createElement("THEAD");
-    tabla_mes.appendChild(cabecera);
-    let fila = document.createElement("TR");
-    cabecera.appendChild(fila);
-    for (d = 0; d < 7; d++) {
-      let dia = document.createElement("TH");
-      dia.innerText = dia_text[d];
-      fila.appendChild(dia);
-    }
-    //Cuerpo
-    let cuerpo = document.createElement("TBODY");
-    tabla_mes.appendChild(cuerpo);
-    for (f = 0; f < 6; f++) {
-      let fila = document.createElement("TR");
-      cuerpo.appendChild(fila);
-      for (d = 0; d < 7; d++) {
-        let dia = document.createElement("TD");
-        dia.innerText = "";
-        fila.appendChild(dia);
-      }     
-    }    
-  }
 }
 
-numerar();
-
-function numerar() {
-  for (i = 1; i < 366; i++) {
-    let fecha = fechaPorDia(2017, i);
-    let mes = fecha.getMonth();
-    let select_tabla = document.getElementsByClassName('tabla_mes')[mes];
-    let dia = fecha.getDate()
-    let dia_semana = fecha.getDay();
-    if (dia == 1) {var sem = 0;}
-    select_tabla.children[2].children[sem].children[dia_semana].innerText = dia;
-    if (dia_semana == 6) { sem = sem + 1; }
-  }
+function saludo_3()
+{
+  alert('Esto vale para la funcion ejecuta');
 }
-
-function fechaPorDia(año, dia) {
-  var date = new Date(año, 0);
-  return new Date(date.setDate(dia));
+function saludo_4()
+{
+  alert('Esto vale para ejecuta 2');
 }
+function ejecuta() {
+  
+  document.getElementsByTagName("p")[0].onclick = saludo_3;
+}
+function ejecuta_1()
+{
+  document.getElementById("importante").onclick = saludo_4;
+}
+window.onload = saludo_2;
+window.onload = ejecuta;
+window.onload = ejecuta_1;
